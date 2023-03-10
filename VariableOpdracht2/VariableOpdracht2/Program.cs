7e8fdcba71c76e44d18482f1ee5e987e78556fc5
Luuk <35917@ma-web.nl>
2023-03-10 12:56:38 +0100

    variabelen klaar

---
+++ VariableOpdracht2/VariableOpdracht2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class Program
+    {
+        //class variables & gebruik
+        static void Main(string[] args)
+        {
+
+            //ga naar de file TwitchStream.cs in je solution explorer in visual studio
+            TwitchStream twitchStream = new TwitchStream();
+
+            twitchStream.naam = "Jake ";
+            twitchStream.laasteNaam = "Flow ";
+            twitchStream.game = "Gta 5 ";
+            twitchStream.follwers = 3459;
+            twitchStream.automod = true;
+            //geef nu de stream de volgende waardes:
+            //name, waarde: je favoriete streamer
+            //lastStreamName, waarde: de streamname van de laatste keer
+            //streamGame, waarde een nieuwe instance van Game (new Game("naam van de game hier"})
+            //followers, waarde: hoeveel followers
+            //automod, waarde: staat automod aan?
+
+
+            Console.WriteLine(twitchStream.naam + twitchStream.laasteNaam + twitchStream.game + twitchStream.follwers + " " + twitchStream.automod);
+        }
+    }
+}
---
+++ VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class TwitchStream
+    {
+        //maak hier class variables aan:
+        // type=string, naam: name,
+        // type=string, naam: lastStreamName
+        // type=Game, naam: streamGame
+        // type=int, naam: followers
+        // type=bool, naam: automod
+
+        internal string naam;
+        internal string laasteNaam;
+        internal string game;
+        internal int follwers;
+        internal bool automod;
+
+
+    }
+}
